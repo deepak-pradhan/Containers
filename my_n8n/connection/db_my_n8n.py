c829c400363db3d40cef13cc4531c6684fcbf2d2
deepak-pradhan <deepak.pradhan@gmail.com>
2025-01-20 15:00:10 -0500

    0.0.4
    cleaned extra lines

--- my_n8n/connection/db_my_n8n.py
+++ my_n8n/connection/db_my_n8n.py
@@ -3,24 +3,12 @@ import psycopg
 import sqlite3
 
 class DBConnections:
+    app = sqlite3.connect("app.db")
     source = sqlite3.connect("source.db")
     target = psycopg.connect("host=localhost port=5433 dbname=postgres user=n8n_user password=n8n_password")
-    app = sqlite3.connect("app.db")
-
-class SourceDBConnection:
-    con = sqlite3.connect("source.db")
-
-class TargetDBConnection:
-    con = psycopg.connect("host=localhost port=5433 dbname=postgres user=n8n_user password=n8n_password")
-
-class AppDBConnection:
-    con = sqlite3.connect("app.db")
-
-def get_db_app():
+def get_db_app() -> DBConnections:
     return DBConnections.app
-
 def get_db_source():
     return DBConnections.source
-
 def get_db_target():
     return DBConnections.target

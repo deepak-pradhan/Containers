5ba48c6faaf222a41447d103b8b615faa8e755f5
deepak-pradhan <deepak.pradhan@gmail.com>
2025-01-20 02:10:44 -0500

    0.0.3
    formatting

--- my_n8n/model/_base.py
+++ my_n8n/model/_base.py
@@ -6,14 +6,15 @@ sys.path.append("./../")
 from pydantic import BaseModel
 from typing import TypeVar, ClassVar, Tuple
 from rich import print
-from loguru import logger as log
 from my_n8n.connection.db_my_n8n import get_db_app, get_db_source, get_db_target
 
 T = TypeVar("T", bound="_Base")
+
 class _Base(BaseModel):
     table_name: str = "base_s"
     columns: ClassVar[Tuple[str, ...]] = ()
-    _sample ={}
+
+    _sample = {}
     model_config = {
         "extra": "ignore",
         "arbitrary_types_allowed": True,
@@ -28,20 +29,22 @@ class _Base(BaseModel):
     
     @property
     def _table_s(self) -> str:
-        return self.__class__.__name__.lower() + '_s'
-    
+        return f"{self.__class__.__name__.lower()}_s"
+
     @property
     def _table_t(self) -> str:
-        return self.__class__.__name__.lower() + '_t'
+        return f"{self.__class__.__name__.lower()}_t"
 
     # ## Helpers
     def _inspect(cls) -> None:
-        """self inspects"""
+        """Inspect self"""
         print('Source Table:', cls._table_s)
         print('Target Table:', cls._table_t)
         print(f'Columns: {cls.columns}')
+        
         print(f'Sample: {cls._sample}')
         print(f'Model Config: {cls.model_config}')
+
         print('\nModel:', cls)
         print('\nModel dump json:', cls.model_dump_json())
         print('\nModeel json schema:', cls.model_json_schema())     
@@ -61,7 +64,7 @@ class _Base(BaseModel):
         cur.execute(query)
         con.commit()
     def _auto_create_app_table(self, model: T) -> None:
-        q = f'CREATE TABLE IF NOT EXISTS {model.table_name} ({", ".join(model.columns)})'
+        q = f'CREATE TABLE IF NOT EXISTS {model.table_name} ({", ".join(self.columns)})'
         self._exec_app_dcl(q)
     def _auto_drop_app_table(self, model: T) -> None:
         q = f'DROP TABLE IF EXISTS {model.table_name}'
@@ -72,15 +75,14 @@ class _Base(BaseModel):
     def _auto_drop_source_table(self, model: T) -> None:
         q = f'DROP TABLE IF EXISTS {model._table_s}'
         self._exec_source_dcl(q)
-    def _auto_create_target_table(self, model: T) -> None:
-        q = f'CREATE TABLE IF NOT EXISTS {model._table_t} ({", ".join(model.columns)})'
+    def _auto_create_target_table(self) -> None:
+        q = f'CREATE TABLE IF NOT EXISTS {self._table_t} ({", ".join(self.columns)})'
         self._exec_source_dcl(q)    
-    def _auto_drop_target_table(self, model: T) -> None:
-        q = f'DROP TABLE IF EXISTS {model._table_t}'
+    def _auto_drop_target_table(self) -> None:
+        q = f'DROP TABLE IF EXISTS {self._table_t}'
         self._exec_source_dcl(q)
 
 # ##
 if __name__ == "__main__":
     m = _Base()
     m._inspect()
-    pass
